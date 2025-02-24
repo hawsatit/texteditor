46684b41f9478452f37e1dd99a4e274a07e570e4
hawsatit <hawsatit@132.161.196.225>
2025-02-23 21:32:55 -0600

    Added the condition for TextEditor

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -17,10 +17,10 @@ import java.nio.file.Paths;
  * The driver for the TextEditor Application.
  */
 public class TextEditor {
-    
+
     public static void drawBuffer(GapBuffer buf, Screen screen) throws IOException {
         screen.clear();
-        
+
         int row = 0;
         int col = 0;
         int size = buf.getSize();
@@ -30,12 +30,11 @@ public class TextEditor {
             screen.setCharacter(col, row, new TextCharacter(character));
             col++;
         }
-        
-        screen.setCursorPosition(new TerminalPosition(col, 0));
+
+        screen.setCursorPosition(new TerminalPosition(buf.getCursorPosition(), 0));
         screen.refresh();
     }
-    
-    
+
     public static Screen createScreen() throws IOException {
         Screen screen = new DefaultTerminalFactory().createScreen();
         return screen;
@@ -43,62 +42,64 @@ public class TextEditor {
 
     /**
      * The main entry point for the TextEditor application.
+     *
      * @param args command-line arguments.
      * @throws java.io.IOException
      */
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Usage: java TextEditor <filename>");
             System.exit(1);
         }
-        
+
         GapBuffer buf = new GapBuffer();
         String pathString = args[0];
         System.out.format("Loading %s...\n", pathString);
         Path path = Paths.get(pathString);
-        
-        
-        if (Files.exists(path) && Files.isRegularFile(path)){
+
+        if (Files.exists(path) && Files.isRegularFile(path)) {
             String existingContent = Files.readString(path);
-            for (int i = 0; i < existingContent.length(); i++){
+            for (int i = 0; i < existingContent.length(); i++) {
                 buf.insert(existingContent.charAt(i));
             }
-            
-            Screen screen = createScreen();
-            screen.startScreen();
-            drawBuffer(buf, screen);
-        
-            boolean isRunning = true;
-        
-            while (isRunning) {
-            KeyStroke stroke = screen.readInput();
-                if (stroke.getKeyType() == KeyType.Character){
-                    buf.insert(stroke.getCharacter());
-                    drawBuffer(buf, screen);
-                } else if (stroke.getKeyType() == KeyType.ArrowLeft){
-                   if (buf.getCursorPosition() > 0){
-                       buf.moveLeft();
-                   }
-                   drawBuffer(buf, screen);
-                } else if (stroke.getKeyType() == KeyType.ArrowRight){
-                   if (buf.getCursorPosition() < buf.getSize() - 1){
-                       buf.moveRight();
-                   }
-                   drawBuffer(buf, screen);
-                } else if (stroke.getKeyType() == KeyType.Backspace){
-                    if (buf.getSize() > 0){
-                        buf.delete();
-                    }
-                    drawBuffer(buf, screen);
-                } else if (stroke.getKeyType() == KeyType.Escape){
-                    screen.stopScreen();
-                    isRunning = false;
-                }
-            }
-            
-            Files.writeString(path, buf.toString());
 
-        }        
-           
+            buf.gapStart = buf.getSize();
+        }
+
+        Screen screen = createScreen();
+        screen.startScreen();
+        drawBuffer(buf, screen);
+
+        boolean isRunning = true;
+
+        while (isRunning) {
+            KeyStroke stroke = screen.readInput();
+            if (stroke.getKeyType() == KeyType.Character) {
+                buf.insert(stroke.getCharacter());
+                drawBuffer(buf, screen);
+            } else if (stroke.getKeyType() == KeyType.ArrowLeft) {
+                if (buf.getCursorPosition() > 0) {
+                    buf.moveLeft();
+                }
+                drawBuffer(buf, screen);
+            } else if (stroke.getKeyType() == KeyType.ArrowRight) {
+                if (buf.getCursorPosition() < buf.getSize()) {
+                    buf.moveRight();
+                }
+                drawBuffer(buf, screen);
+            } else if (stroke.getKeyType() == KeyType.Backspace) {
+                if (buf.getSize() > 0) {
+                    buf.delete();
+                }
+                drawBuffer(buf, screen);
+            } else if (stroke.getKeyType() == KeyType.Escape) {
+                screen.stopScreen();
+                isRunning = false;
+            }
+        }
+
+        Files.writeString(path, buf.toString());
+
     }
+
 }
